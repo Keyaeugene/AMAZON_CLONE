43598f03922664518eb93a8ed688787df0692b18
keyaeugene <keyaeugene@gmail.com>
2024-02-21 19:59:32 +0300

    done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const express =  require('express');
 const authRouter = express.Router ();
 
 authRouter.post( '/api/signup', (req, res) => { 
+   const {name, email, password} =  req.body;
     //get data from the client
     //post the data in the database
     //return the data to the user
